dce81d9671c75847367c8f5f316875c6dc92aa44
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-03-29 19:38:12 +0530

    added video-box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import './App.scss';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import AppVideo from './components/AppVideo/AppVideo';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      main content
+      <AppVideo/>
       <Footer/>
     </div>
   );
---
+++ src/components/AppVideo/AppVideo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './AppVideo.scss'
+
+function AppVideo() {
+  return (
+    <div className='app-video'>
+      <div className='app-video-box'>app-video-box</div>
+    </div>
+  )
+}
+
+export default AppVideo;
