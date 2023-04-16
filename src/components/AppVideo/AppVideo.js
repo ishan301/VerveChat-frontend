0b1c497498b8699d221c22a1d69ea858baae1b66
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-04-16 23:01:32 +0530

    login component initialised

--- src/components/AppVideo/AppVideo.js
+++ src/components/AppVideo/AppVideo.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './AppVideo.scss'
 import Header from '../Header/Header';
+import Login from '../Login/Login';
 
 function AppVideo() {
   return (
     <div className='app-video'>
       <Header/>
-      <div className='app-video-box'>app-video-box</div>
+      <div className='app-video-box'>
+        <Login/>
+      </div>
     </div>
   )
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Login.scss";
+
+const Login = () => {
+  return <div className="login_wrapper">Login</div>;
+};
+
+export default Login;
