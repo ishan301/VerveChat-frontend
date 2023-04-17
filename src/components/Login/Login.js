44b1348e26520529500402bb9f4c96aef0ca7c18
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-04-17 23:25:34 +0530

    added google oauth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.scss";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { userInfo } from "../../utils/fetchUser";
+import { useNavigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 const Login = () => {
-  return <div className="login_wrapper">Login</div>;
+  const navigate = useNavigate();
+  const [text, setText] = useState("Please Login to use the application")
+  return userInfo ? (
+    navigate("/")
+  ) : (
+    <div className="login_wrapper">
+      <h3>{text}</h3>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_CLIENT_ID}>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            const userObject = jwtDecode(credentialResponse.credential);
+            console.log(userObject.email);
+          }}
+          onError={() => {
+            setText("Login Failed 😟 please try again...")
+          }}
+        />
+      </GoogleOAuthProvider>
+    </div>
+  );
 };
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
